e7f7924a42e86fcebb8074e717437903faeb8ff4
Mäder Michael <mike@moik.org>
2017-12-08 11:34:46 +0100

    added view template

--- sonnen/urls.py
+++ sonnen/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    url('^$', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('<int:sonnen_id>/', views.detail, name='detail'),
+
+#    url(''),
 ]
--- sonnen/views.py
+++ sonnen/views.py
@@ -1,7 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import SonnenBattery
 
 from django.http import HttpResponse
 
 def index(request):
-    #actualData =
-    return HttpResponse('<h1>Sonnen View homepage</h1>')
+#    all_sonnenData = SonnenBattery.objects.last()
+    all_sonnenData = SonnenBattery.objects.all()
+    context = {'all_sonnen': all_sonnenData}
+    return render(request, 'sonnen/index.html', context)
+
+def detail(request, sonnen_id):
+    sonnenData = SonnenBattery.objects.get(id=sonnen_id)
+    ts = str(sonnenData.timestamp)
+    return HttpResponse('<h2>Details for Sonnen data set ' + str(sonnen_id) + ' </h2>ts: ' + ts + '</h2>')
