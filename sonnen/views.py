453f0d9466516798f760ded690017283c2452561
Mäder Michael <mike@moik.org>
2017-12-11 13:37:21 +0100

    added 404 for Detail page removed static URL path

--- sonnen/views.py
+++ sonnen/views.py
@@ -1,16 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import SonnenBattery
+from django.http import Http404
 
 from django.http import HttpResponse
 
 def index(request):
 #    all_sonnenData = SonnenBattery.objects.last()
     all_sonnenData = SonnenBattery.objects.all()
-    context = {'all_sonnen': all_sonnenData}
-    return render(request, 'sonnen/index.html', context)
+    return render(request, 'sonnen/index.html', {'all_sonnen': all_sonnenData})
 
 def detail(request, sonnen_id):
-    sonnenData = SonnenBattery.objects.get(id=sonnen_id)
-    ts = str(sonnenData.timestamp)
-    return HttpResponse('<h2>Details for Sonnen data set ' + str(sonnen_id) + ' </h2>ts: ' + ts + '</h2>')
+    try:
+        sonnenData = SonnenBattery.objects.get(id=sonnen_id)
+        ts = str(sonnenData.timestamp)
+    except SonnenBattery.DoesNotExist:
+        raise Http404("Sonnen detail does not exist")
+#    return HttpResponse('<h2>Details for Sonnen data set ' + str(sonnen_id) + ' </h2>ts: ' + ts + '</h2>')
+    return render(request, 'sonnen/detail.html', {'all_sonnen': sonnenData})
