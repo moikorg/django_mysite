b975e4b5f5faa965d867f2b9bafa9dbb264f36ee
Mäder Michael <mike@moik.org>
2017-12-13 21:42:49 +0100

    generic views

--- sonnen/urls.py
+++ sonnen/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'sonnen'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:sonnen_id>/', views.detail, name='detail'),
-    path('status/', views.status, name='status')
+#    path('', views.index, name='index'),
+#    path('<int:sonnen_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('status/', views.status, name='status'),
 
-#    url(''),
 ]
--- sonnen/views.py
+++ sonnen/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import SonnenBattery
 from django.http import HttpResponse
-import json
-from django.core import serializers
+from django.views import generic
 
-def index(request):
-    all_sonnenData = SonnenBattery.objects.all()
-    return render(request, 'sonnen/index.html', {'all_sonnen': all_sonnenData})
+# def index(request):
+#     all_sonnenData = SonnenBattery.objects.all()
+#     return render(request, 'sonnen/index.html', {'all_sonnen': all_sonnenData})
+#
+# def detail(request, sonnen_id):
+#     sonnenData = get_object_or_404(SonnenBattery, id=sonnen_id, )
+#     return render(request, 'sonnen/detail.html', {'all_sonnen': sonnenData})
 
-def detail(request, sonnen_id):
-    sonnenData = get_object_or_404(SonnenBattery, id=sonnen_id, )
-    return render(request, 'sonnen/detail.html', {'all_sonnen': sonnenData})
+class DetailView(generic.DetailView):
+    model = SonnenBattery
+    template_name = 'sonnen/detail.html'
+
+class IndexView(generic.ListView):
+    template_name = 'sonnen/index.html'
+    context_object_name = 'all_sonnen'
+
+    def get_queryset(self):
+        return SonnenBattery.objects.all()
 
 def status(request):
     qs = SonnenBattery.objects.latest('timestamp')
