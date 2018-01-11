14093b7a82d5dbeaa1dcd39757608b93689878d3
Mäder Michael <mike@moik.org>
2018-01-11 13:38:21 +0100

    authentication

--- sonnen/views.py
+++ sonnen/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+#from django.shortcuts import render, get_object_or_404
 from .models import SonnenBattery
 from django.http import HttpResponse
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+#from django.views import generic
+#from .forms import UserForm
 
-# def index(request):
-#     all_sonnenData = SonnenBattery.objects.all()
-#     return render(request, 'sonnen/index.html', {'all_sonnen': all_sonnenData})
-#
-# def detail(request, sonnen_id):
-#     sonnenData = get_object_or_404(SonnenBattery, id=sonnen_id, )
-#     return render(request, 'sonnen/detail.html', {'all_sonnen': sonnenData})
+
 
 class DetailView(generic.DetailView):
     model = SonnenBattery
@@ -21,7 +19,7 @@ class IndexView(generic.ListView):
     template_name = 'sonnen/index.html'
     context_object_name = 'all_sonnen'
     paginate_by = 50
-    queryset = SonnenBattery.objects.order_by('-timestamp')
+    queryset = SonnenBattery.objects.order_by('+timestamp')
 
     def get_queryset(self):
         return SonnenBattery.objects.all()
@@ -36,3 +34,7 @@ def status(request):
                'gridConsumption': qs.gridConsumption,
                }
     return render(request, 'sonnen/status.html', context)
+
+
+class UserFormView(View):
+    form_cl
