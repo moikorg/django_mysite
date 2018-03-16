eaef402b5d152c53cf8842b9fc4522b1db6aeb4e
Michael Mäder <mike@moik.org>
2018-03-16 21:35:06 +0100

    try to get authentication working

--- sonnen/urls.py
+++ sonnen/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'sonnen'
 
@@ -7,5 +8,4 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('status/', views.status, name='status'),
-
 ]
