9b8f1abae2a01ae01b57928dfb57a634fed546c3
Mäder Michael <mike@moik.org>
2018-06-02 22:22:41 +0200

    remove the frequency

--- sonnen/models.py
+++ sonnen/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SonnenBattery(models.Model):
     consumption = models.SmallIntegerField()
-    frequency = models.PositiveSmallIntegerField()
+    #frequency = models.PositiveSmallIntegerField()
     # gridConsumption:
     #   negativ value = energy consumption from grid
     #   positiv value = energy feed-in to the grid
